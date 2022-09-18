5914428e458c8d018582a9f62ee31ef0b7b7a234
ubuntu <ubuntu@localhost.localdomain>
2022-09-18 23:42:33 +0800

    :sparkles: add new function

---
+++ gopl.io/ch3/exc6/control_structure_idiom_6.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	exit := make(chan interface{})
+
+	go func() {
+	loop:
+		for {
+			select {
+			case <-time.After(time.Second):
+				fmt.Println("tick")
+			case <-exit:
+				fmt.Println("exiting...")
+				break loop
+			}
+		}
+		fmt.Println("exit!")
+	}()
+
+	time.Sleep(3 * time.Second)
+	exit <- struct{}{}
+	time.Sleep(3 * time.Second)
+}
